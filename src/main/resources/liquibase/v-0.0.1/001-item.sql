2f39d01d09c78c15b2946747e87a3a4b3cd3ae91
Vladimir Andreev <vladimirandreev1987@mail.com>
2024-02-27 10:07:44 +0100

    Changed. Error with liquibase.

--- src/main/resources/liquibase/v-0.0.1/001-item.sql
+++ src/main/resources/liquibase/v-0.0.1/001-item.sql
@@ -1,8 +1,8 @@
 --liquibase formatted sql
 --changeset id:1588510956469-1
 CREATE TABLE IF NOT EXISTS item (
-    id        UUID                    NOT NULL CONSTRAINT PK_item PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id        UUID                    NOT NULL CONSTRAINT PK_item PRIMARY KEY,
     name      VARCHAR UNIQUE          NOT NULL,
-    added     DATE DEFAULT NOW()      NOT NULL,
+    added     DATE                    NOT NULL,
     available BOOLEAN   DEFAULT TRUE  NOT NULL
 );
